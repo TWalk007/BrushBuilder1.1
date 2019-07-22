b20b10b5902121ef9d67e5a65f31ade1db51b353
TWalk007 <walkertb03@gmail.com>
2019-07-22 11:57:22 -0400

    Added Error Codes
    Makes sure the user inputs a valid code into the input field.

---
+++ Assets/Scripts/ErrorDisplayManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ErrorDisplayManager : MonoBehaviour
+{
+    public GameObject errorPanel;
+    public TextMeshProUGUI errorMessage;
+
+    public void CloseDialogue()
+    {
+        errorPanel.SetActive(false);
+    }
+}
